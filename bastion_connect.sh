a677220b6207aaa0668c1e3d659a980d415e95b8
LiranMil <liran.milman1@gmail.com>
2024-08-21 22:32:13 +0300

    commit

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,29 +1,27 @@
+
 #!/bin/bash
 
-#לבדוק האם אם קי-פאט חסר
 if [ -z "$KEY_PATH" ]; then
-    echo "Error: KEY_PATH environment variable is required."
+    echo "KEY_PATH env var is expected"
     exit 5
 fi
 
-#בודק האם באמת יש IP כלשהו
 if [ $# -lt 1 ]; then
-    echo "Error: Please provide the bastion (public instance) IP address."
+    echo "Please provide bastion IP address"
     exit 5
 fi
 
-#הגדרת משתנים
 PUBLIC_IP=$1
 PRIVATE_IP=$2
-COMMAND=${@:3}
+COMMAND=$3
 
-#
 if [ -z "$PRIVATE_IP" ]; then
+
     ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
 else
     if [ -z "$COMMAND" ]; then
-        ssh -i "$KEY_PATH" -A ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP"
+        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@"$PRIVATE_IP"
     else
-        ssh -i "$KEY_PATH" -A ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP" "$COMMAND"
+        ssh -i "$KEY_PATH" -t ubuntu@"$PUBLIC_IP" ssh -i /home/ubuntu/.ssh/id_rsa ubuntu@"$PRIVATE_IP" "$COMMAND"
     fi
 fi
