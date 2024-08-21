61fa05c3720b46aeef91cbb54528e965ebe259ae
LiranMil <liran.milman1@gmail.com>
2024-08-21 22:00:07 +0300

    Update bastion_connect.sh

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-#לבדוק האם אם קי-פאט חסר
+# Check if KEY_PATH environment variable is set
 if [ -z "$KEY_PATH" ]; then
     echo "Error: KEY_PATH environment variable is required."
     exit 5
 fi
 
-#בודק האם באמת יש IP כלשהו
+# Check if a public IP address is provided
 if [ $# -lt 1 ]; then
     echo "Error: Please provide the bastion (public instance) IP address."
     exit 5
 fi
 
-#הגדרת משתנים
+# Define variables
 PUBLIC_IP=$1
 PRIVATE_IP=$2
 COMMAND=${@:3}
 
-#
+# Check if PRIVATE_IP is provided
 if [ -z "$PRIVATE_IP" ]; then
+    # Connect to the public instance only
     ssh -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
 else
     if [ -z "$COMMAND" ]; then
+        # Connect to the private instance through the public instance
         ssh -i "$KEY_PATH" -A ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP"
     else
+        # Execute a command on the private instance through the public instance
         ssh -i "$KEY_PATH" -A ubuntu@"$PUBLIC_IP" ssh -i "$KEY_PATH" ubuntu@"$PRIVATE_IP" "$COMMAND"
     fi
 fi
