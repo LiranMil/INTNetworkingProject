edae67011e4e8ef005b920ded24c5409b92cb65f
LiranMil <liran.milman1@gmail.com>
2024-08-26 01:13:36 +0300

    Done !

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,4 +1,3 @@
-<<comment
 #!/bin/bash
 
 # Ensure correct usage
@@ -52,22 +51,3 @@ mv $NEW_KEY_PATH $HOME/.ssh/id_rsa
 mv $PUBLIC_KEY_PATH $HOME/.ssh/id_rsa.pub
 
 echo "SSH key rotation completed successfully."
-comment
-#!/bin/bash
-
-#Rotates the keys of the private instance
-CURRENT_KEY_PATH="/home/ubuntu/.ssh/id_rsa"
-ROLLED_KEY_PATH="/home/ubuntu/.ssh/id_rsa_new"
-private_server_ip=${1}
-
-if [[ -z private_server_ip ]]
-then
-  echo "Error : Private server ip unspecified."
-  exit 1
-else
-  #Create new keyfile, overriding the previous one, redirecting output to /dev/null
-  ssh-keygen -q -f ${ROLLED_KEY_PATH} -N "" -t rsa <<< y > /dev/null
-  scp -q -i ${CURRENT_KEY_PATH} "${ROLLED_KEY_PATH}.pub" ubuntu@${private_server_ip}:"${ROLLED_KEY_PATH}.pub" #Copy new public key to private instance
-  ssh -i ${CURRENT_KEY_PATH} ubuntu@${private_server_ip} "cat ${ROLLED_KEY_PATH}.pub > ~/.ssh/authorized_keys" #Perform key rotation in public instance. ssh session will be disconnected.
-  cp ${ROLLED_KEY_PATH} ${CURRENT_KEY_PATH} #We're back to the public instance. Set current key to the rolled key.
-fi
