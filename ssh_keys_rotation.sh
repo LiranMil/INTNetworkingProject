caff0fa11354123518de9fd3a402371c0484e646
LiranMil <liran.milman1@gmail.com>
2024-08-25 18:17:57 +0300

    work!!

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,46 +1,40 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-   echo "please provide private-instance-ip"
-   exit 1
-fi
-
-PRIVATE_INSTANCE_IP="$1"
-NEW_KEY_NAME="id_rsa"
-NEW_KEY_PATH="$HOME/.ssh/$NEW_KEY_NAME"
-OLD_KEY_PATH="$NEW_KEY_PATH"
-mv "$OLD_KEY_PATH" "$OLD_KEY_PATH".old
-OLD_KEY_PATH="$OLD_KEY_PATH".old
-OLD_KEY_PATH_PUB="$HOME/.ssh/$NEW_KEY_NAME.pub"
-echo "$OLD_KEY_PATH_PUB"
-mv "$OLD_KEY_PATH_PUB" "$OLD_KEY_PATH_PUB".old
-OLD_KEY_PATH_PUB="$OLD_KEY_PATH_PUB".old
-echo "$OLD_KEY_PATH_PUB"
-
-if ! ssh-keygen -t rsa -b 2048 -f "$NEW_KEY_PATH" -N ""; then
-   echo "Failed to generate SSH key"
-   exit 1
-fi
-echo "$NEW_KEY_PATH"
-if ! sudo chmod 400 "$NEW_KEY_PATH"; then
-   echo "Failed to set permissions on the key"
-   exit 1
-fi
-echo "$OLD_KEY_PATH"
-echo "$NEW_KEY_PATH"
-
-if ! ssh -i "$OLD_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" "cat > ~/.ssh/authorized_keys" < "$NEW_KEY_PATH.pub"; then
-   echo "Failed to copy key to the private machine"
-   exit 1
-fi
-echo "Testing SSH connection with the new key..."
-if ! ssh -i "$NEW_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" -o StrictHostKeyChecking=no "echo 'Connection successful with new key'"; then
-    echo "Failed to connect with the new key"
+# Check if the private instance IP is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <private-instance-ip>"
     exit 1
 fi
-echo "$NEW_KEY_PATH"
-echo "$OLD_KEY_PATH"
 
-echo "Key rotation successful. You can now use the new key to access the private instance."
-rm -f "$OLD_KEY_PATH" "$OLD_KEY_PATH_PUB"
+PRIVATE_IP="$1"
+#cp "$HOME/key.pem" "$HOME/.ssh/id_rsa_old"
 
+
+PRIVATE_KEY_PATH="$HOME/.ssh/id_rsa"   # Path to store the new key
+PUBLIC_KEY_PATH="$PRIVATE_KEY_PATH".pub
+
+OLD_PRIVATE_KEY_PATH="$PRIVATE_KEY_PATH".old  # Path to your old key
+mv "$PRIVATE_KEY_PATH" "$OLD_PRIVATE_KEY_PATH"
+
+OLD_KEY_PATH_PUB="$PUBLIC_KEY_PATH".old
+mv "$PUBLIC_KEY_PATH" "$OLD_KEY_PATH_PUB"
+
+# Generate a new SSH key pair
+ssh-keygen -t rsa -b 4096 -f "$PRIVATE_KEY_PATH" -N ""  -C "Key rotated on $(date)" > /dev/null
+chmod 400 "$PRIVATE_KEY_PATH"
+
+# Ensure the old key exists before using it
+#if [ ! -f "$OLD_KEY_PATH" ]; then
+#    echo "Old key not found at $OLD_KEY_PATH"
+#    exit 1
+#fi
+
+if ! ssh -i "$OLD_PRIVATE_KEY_PATH" ubuntu@"$PRIVATE_IP" "cat > ~/.ssh/authorized_keys" < "$PUBLIC_KEY_PATH"; then
+  echo "Failed to copy public key to the private machine"
+  exit 1
+fi
+
+
+# Test SSH connection with the new key
+echo "connect"
+ssh -o StrictHostKeyChecking=no -i "$NEW_KEY_PATH" ubuntu@"$PRIVATE_IP"
