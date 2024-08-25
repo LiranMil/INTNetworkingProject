a609b1808ff61798a40ce7146ee396b2ab22f76a
LiranMil <liran.milman1@gmail.com>
2024-08-25 17:15:37 +0300

    work!

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -25,10 +25,22 @@ if ! sudo chmod 400 "$NEW_KEY_PATH"; then
    exit 1
 fi
 
-# Backup the authorized_keys and clear it
+echo "Old key path: $OLD_KEY_PATH"
+echo "New key path: $NEW_KEY_PATH"
+
+# Test SSH connection using old key
+echo "Testing SSH connection with the old key..."
+if ! ssh -i "$OLD_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" "echo 'Old key test successful'"; then
+   echo "Failed to connect with the old key"
+   exit 1
+fi
+
+# Backup and clear authorized_keys
+echo "Backing up and clearing authorized_keys..."
 ssh -i "$OLD_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" "cp ~/.ssh/authorized_keys ~/.ssh/authorized_keys.backup && echo '' > ~/.ssh/authorized_keys"
 
 # Copy the new public key to the private instance's authorized_keys file
+echo "Copying new key to authorized_keys..."
 if ! ssh -i "$OLD_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" "echo '$(cat $NEW_KEY_PATH.pub)' > ~/.ssh/authorized_keys"; then
    echo "Failed to copy key to the private machine"
    exit 1
@@ -41,6 +53,5 @@ if ! ssh -i "$NEW_KEY_PATH" "ubuntu@$PRIVATE_INSTANCE_IP" -o StrictHostKeyChecki
     exit 1
 fi
 
-# Clean up by removing the old keys
 echo "Key rotation successful. You can now use the new key to access the private instance."
 rm -f "$OLD_KEY_PATH" "$OLD_KEY_PATH_PUB"
