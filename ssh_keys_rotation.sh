87656f56cb4f9cfc41c600ae67e1bb55636e000b
LiranMil <liran.milman1@gmail.com>
2024-08-26 21:04:55 +0300

    god-help-me

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Ensure correct usage
 if [ $# -ne 1 ]; then
@@ -13,36 +14,36 @@ PUBLIC_KEY_PATH="$NEW_KEY_PATH.pub"
 OLD_KEY_PATH="$HOME/.ssh/id_rsa"
 
 # Generate a new SSH key pair
-ssh-keygen -t rsa -b 4096 -f $NEW_KEY_PATH -N ""
-chmod 600 $NEW_KEY_PATH
+ssh-keygen -t rsa -b 4096 -f "$NEW_KEY_PATH" -N ""
+chmod 600 "$NEW_KEY_PATH"
 
 # Copy the new public key to the authorized_keys on the private instance
-NEW_PUBLIC_KEY=$(cat $PUBLIC_KEY_PATH)
+NEW_PUBLIC_KEY=$(cat "$PUBLIC_KEY_PATH")
 ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP "echo '$NEW_PUBLIC_KEY' >> ~/.ssh/authorized_keys"
 
 # Verify the new key works
-ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
-if [ $? -ne 0 ]; then
+if ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'; then
+  echo "New key is working."
+else
   echo "Failed to connect to the private instance using the new key."
   exit 1
 fi
 
 # Remove the old key from authorized_keys on the private instance
-OLD_PUBLIC_KEY=$(cat $OLD_KEY_PATH.pub)
-ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP "grep -v '$OLD_PUBLIC_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
+OLD_PUBLIC_KEY=$(cat "$OLD_KEY_PATH.pub")
+ssh -i "$NEW_KEY_PATH" ubuntu@$PRIVATE_IP "grep -vF '$OLD_PUBLIC_KEY' ~/.ssh/authorized_keys > ~/.ssh/authorized_keys.tmp && mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys"
 
 # Verify the old key no longer works
-ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'
-if [ $? -eq 0 ]; then
+if ssh -i "$OLD_KEY_PATH" ubuntu@$PRIVATE_IP 'exit'; then
   echo "Old key is still valid, which shouldn't be the case."
   exit 1
 fi
 
-# Remove old key from the public instance
-rm -f $OLD_KEY_PATH $OLD_KEY_PATH.pub
+# Remove old key from the local machine
+rm -f "$OLD_KEY_PATH" "$OLD_KEY_PATH.pub"
 
 # Replace the old key with the new key locally
-mv $NEW_KEY_PATH $HOME/.ssh/id_rsa
-mv $PUBLIC_KEY_PATH $HOME/.ssh/id_rsa.pub
+mv "$NEW_KEY_PATH" "$HOME/.ssh/id_rsa"
+mv "$PUBLIC_KEY_PATH" "$HOME/.ssh/id_rsa.pub"
 
 echo "SSH key rotation completed successfully."
